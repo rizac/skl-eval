ed6e199c17500536cd68da3d969622e0c98494c6
rizac <riccardo.zaccarelli@gmail.com>
2021-09-21 09:15:03 +0200

    fix imports and tests

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU License',
 
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
